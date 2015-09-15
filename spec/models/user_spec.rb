af39551f222179bb1abde62d13c7dde84ffbda2d
lessless <me@lessless.pp.ua>
2015-09-15 08:31:52 +0700

    added list scaffold

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:user) { FactoryGirl.build_stubbed(:user) }
+
+  it "have todo list" do
+    first_list = "First List"
+    user.lists << FactoryGirl.build_stubbed(:list, name: first_list)
+    expect(user.lists.map(&:title)).to eq([first_list])
+  end
 end

85801f6bf784ef983fdd8bf752a7e1c139336259
lessless <me@lessless.pp.ua>
2015-09-15 14:19:26 +0700

    added smoky List model test

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
+  let (:create_params)  { FactoryGirl.attributes_for(:list) }
+
+  it "should have an author" do
+    list = List.create(create_params)
+    expect(list.errors[:author_id].length).to eq(1)
+  end
 end
